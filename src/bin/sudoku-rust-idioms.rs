be0318c8f4438a39fcd1377b6c49b5e52a4da2a6
Joshua Benuck <joshua_benuck@yahoo.com>
2020-07-06 16:04:11 -0700

    initial support for display to a terminal

--- src/bin/sudoku-rust-idioms.rs
+++ src/bin/sudoku-rust-idioms.rs
@@ -1,3 +1,4 @@
+use clap::{App, Arg};
 use std::env;
 
 #[derive(Clone, PartialEq)]
@@ -95,6 +96,14 @@ impl Board {
         Board { values }
     }
 
+    fn choose_all(&mut self, givens: &str) {
+        for (i, given) in givens.chars().enumerate() {
+            if given != '.' {
+                self.choose(i, given);
+            }
+        }
+    }
+
     fn choose(&mut self, i: usize, choice: char) {
         self.values[i] = Value::Given(choice)
     }
@@ -132,9 +141,87 @@ impl Board {
             })
             .collect()
     }
+
+    fn rows(&self) -> [[usize; 9]; 9] {
+        [
+            // rows
+            [0, 1, 2, 9, 10, 11, 18, 19, 20],
+            [3, 4, 5, 12, 13, 14, 21, 22, 23],
+            [6, 7, 8, 15, 16, 17, 24, 25, 26],
+            [27, 28, 29, 36, 37, 38, 45, 46, 47],
+            [30, 31, 32, 39, 40, 41, 48, 49, 50],
+            [33, 34, 35, 42, 43, 44, 51, 52, 53],
+            [54, 55, 56, 63, 64, 65, 72, 73, 74],
+            [57, 58, 59, 66, 67, 68, 75, 76, 77],
+            [60, 61, 62, 69, 70, 71, 78, 79, 80],
+        ]
+    }
+
+    fn cols(&self) -> [[usize; 9]; 9] {
+        [
+            // columns
+            [0, 3, 6, 27, 30, 33, 54, 57, 60],
+            [1, 4, 7, 28, 31, 34, 55, 58, 61],
+            [2, 5, 8, 29, 32, 35, 56, 59, 62],
+            [9, 12, 15, 36, 39, 42, 63, 66, 69],
+            [10, 13, 16, 37, 40, 43, 64, 67, 70],
+            [11, 14, 17, 38, 41, 44, 65, 68, 71],
+            [18, 21, 24, 45, 48, 51, 72, 75, 78],
+            [19, 22, 25, 46, 49, 52, 73, 76, 79],
+            [20, 23, 26, 47, 50, 53, 74, 77, 80],
+        ]
+    }
+
+    fn squares(&self) -> [[usize; 9]; 9] {
+        [
+            // squares
+            [0, 1, 2, 3, 4, 5, 6, 7, 8],
+            [9, 10, 11, 12, 13, 14, 15, 16, 17],
+            [18, 19, 20, 21, 22, 23, 24, 25, 26],
+            [27, 28, 29, 30, 31, 32, 33, 34, 35],
+            [36, 37, 38, 39, 40, 41, 42, 43, 44],
+            [45, 46, 47, 48, 49, 50, 51, 52, 53],
+            [54, 55, 56, 57, 58, 59, 60, 61, 62],
+            [63, 64, 65, 66, 67, 68, 69, 70, 71],
+            [72, 73, 74, 75, 76, 77, 78, 79, 80],
+        ]
+    }
+
+    fn subsets(&self) -> Vec<[usize; 9]> {
+        [self.rows(), self.cols(), self.squares()].concat()
+    }
+
+    fn solve(&mut self) {
+        // remove choices eliminated by the at-most-one rule
+        for subset in self.subsets().iter() {
+            for i in subset {
+                if let Some(d) = self.values[*i].given() {
+                    let indexes = self.indexes_of(&subset, d).collect();
+                    self.eliminate(indexes, d, Rule::AtMostOne);
+                }
+            }
+        }
+
+        // identify choices mandated by the at-least-one rule
+        for subset in self.subsets().iter() {
+            for digit in "0123456789".chars() {
+                let mut indexes = self.indexes_of(&subset, digit);
+                let possibly_unique = indexes.next();
+                if possibly_unique.is_some() && indexes.next().is_none() {
+                    drop(indexes);
+                    let possibly_unique = possibly_unique.unwrap();
+                    self.values[possibly_unique].keep(digit, Rule::AtLeastOne);
+                }
+            }
+        }
+    }
 }
 
-fn main() {
+fn html(board: &mut Board) {
+    let script = "sudoku-rust-idioms.html"; //   my $script = $ENV{SCRIPT_NAME}; $script =~ s/.*\///;
+    board.choose_all(&env::var("QUERY_STRING").unwrap_or_default());
+    board.solve();
+
     println!(
         r#"
 <html>
@@ -144,74 +231,6 @@ fn main() {
 <body>
 "#
     );
-
-    let script = "sudoku-rust-idioms.html"; //   my $script = $ENV{SCRIPT_NAME}; $script =~ s/.*\///;
-    let mut board = Board::new();
-    for (i, given) in env::var("QUERY_STRING")
-        .unwrap_or_default()
-        .chars()
-        .enumerate()
-    {
-        if given != '.' {
-            board.choose(i, given);
-        }
-    }
-
-    let subsets: [[usize; 9]; 27] = [
-        // rows
-        [0, 1, 2, 9, 10, 11, 18, 19, 20],
-        [3, 4, 5, 12, 13, 14, 21, 22, 23],
-        [6, 7, 8, 15, 16, 17, 24, 25, 26],
-        [27, 28, 29, 36, 37, 38, 45, 46, 47],
-        [30, 31, 32, 39, 40, 41, 48, 49, 50],
-        [33, 34, 35, 42, 43, 44, 51, 52, 53],
-        [54, 55, 56, 63, 64, 65, 72, 73, 74],
-        [57, 58, 59, 66, 67, 68, 75, 76, 77],
-        [60, 61, 62, 69, 70, 71, 78, 79, 80],
-        // columns
-        [0, 3, 6, 27, 30, 33, 54, 57, 60],
-        [1, 4, 7, 28, 31, 34, 55, 58, 61],
-        [2, 5, 8, 29, 32, 35, 56, 59, 62],
-        [9, 12, 15, 36, 39, 42, 63, 66, 69],
-        [10, 13, 16, 37, 40, 43, 64, 67, 70],
-        [11, 14, 17, 38, 41, 44, 65, 68, 71],
-        [18, 21, 24, 45, 48, 51, 72, 75, 78],
-        [19, 22, 25, 46, 49, 52, 73, 76, 79],
-        [20, 23, 26, 47, 50, 53, 74, 77, 80],
-        // squares
-        [0, 1, 2, 3, 4, 5, 6, 7, 8],
-        [9, 10, 11, 12, 13, 14, 15, 16, 17],
-        [18, 19, 20, 21, 22, 23, 24, 25, 26],
-        [27, 28, 29, 30, 31, 32, 33, 34, 35],
-        [36, 37, 38, 39, 40, 41, 42, 43, 44],
-        [45, 46, 47, 48, 49, 50, 51, 52, 53],
-        [54, 55, 56, 57, 58, 59, 60, 61, 62],
-        [63, 64, 65, 66, 67, 68, 69, 70, 71],
-        [72, 73, 74, 75, 76, 77, 78, 79, 80],
-    ];
-
-    // remove choices eliminated by the at-most-one rule
-    for subset in subsets.iter() {
-        for i in subset {
-            if let Some(d) = board.values[*i].given() {
-                let indexes = board.indexes_of(&subset, d).collect();
-                board.eliminate(indexes, d, Rule::AtMostOne);
-            }
-        }
-    }
-
-    // identify choices mandated by the at-least-one rule
-    for subset in subsets.iter() {
-        for digit in "0123456789".chars() {
-            let mut indexes = board.indexes_of(&subset, digit);
-            let possibly_unique = indexes.next();
-            if possibly_unique.is_some() && indexes.next().is_none() {
-                drop(indexes);
-                let possibly_unique = possibly_unique.unwrap();
-                board.values[possibly_unique].keep(digit, Rule::AtLeastOne);
-            }
-        }
-    }
 
     // display board as table of tables of hyperlinked choices
     fn table(first: String) -> String {
@@ -285,4 +304,59 @@ fn main() {
         script,
         "................................................................................."
     );
+}
+
+fn terminal(board: &mut Board) {
+    let mut rows = "123456789".chars();
+    let cols = "123456789".chars();
+    print!("  ");
+    for (i, col) in cols.enumerate() {
+        print!("{}", col);
+        if i != 0 && (i + 1) % 3 == 0 {
+            print!(" ")
+        }
+    }
+    println!("");
+    for (i, row) in board.rows().iter().enumerate() {
+        print!("{} ", rows.next().unwrap());
+        for (j, col) in row.iter().enumerate() {
+            match &board.values[*col] {
+                Value::Choices(choices) => {
+                    print!("?");
+                }
+                Value::Given(choice) => {
+                    print!("{}", choice);
+                }
+            }
+            if j != 0 && (j + 1) % 3 == 0 {
+                print!(" ");
+            }
+        }
+        println!("");
+        if i != 1 && (i + 1) % 3 == 0 {
+            println!("");
+        }
+    }
+}
+
+fn main() {
+    let matches = App::new("sudoku-rust-idioms")
+        .arg(
+            Arg::with_name("terminal")
+                .long("terminal")
+                .short("t")
+                .takes_value(true),
+        )
+        .get_matches();
+
+    let mut board = Board::new();
+
+    if matches.is_present("terminal") {
+        let givens = matches.value_of("terminal").unwrap();
+        board.choose_all(&givens);
+        board.solve();
+        terminal(&mut board);
+    } else {
+        html(&mut board);
+    }
 }
